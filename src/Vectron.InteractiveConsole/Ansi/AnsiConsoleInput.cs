b286fc7e7c93815995a59b0369c39b748d90f0e4
Vectron <thijs_bloebaum@hotmail.com>
2025-12-16 17:45:10 +0100

    IDE0032: Use auto property

--- src/Vectron.InteractiveConsole/Ansi/AnsiConsoleInput.cs
+++ src/Vectron.InteractiveConsole/Ansi/AnsiConsoleInput.cs
@@ -12,7 +12,6 @@ internal sealed class AnsiConsoleInput : IConsoleInput
     private readonly IConsoleOutput consoleOutput;
     private readonly string inputColor;
     private readonly ConsoleInputOptions settings;
-    private string currentInput = string.Empty;
 
     /// <summary>
     /// Initializes a new instance of the <see cref="AnsiConsoleInput"/> class.
@@ -21,6 +20,7 @@ internal sealed class AnsiConsoleInput : IConsoleInput
     /// <param name="consoleOutput">A <see cref="IConsoleOutput"/> instance.</param>
     public AnsiConsoleInput(IOptions<ConsoleInputOptions> options, IConsoleOutput consoleOutput)
     {
+        CurrentInput = string.Empty;
         this.consoleOutput = consoleOutput;
         settings = options.Value;
         inputColor = AnsiHelper.GetAnsiEscapeCode(settings.InputTextColor);
@@ -30,10 +30,10 @@ internal sealed class AnsiConsoleInput : IConsoleInput
     /// <inheritdoc/>
     public string CurrentInput
     {
-        get => currentInput;
+        get;
         set
         {
-            currentInput = value;
+            field = value;
             UpdateText();
         }
     }
